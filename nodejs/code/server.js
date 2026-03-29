a4d78bac6e02faea508ed52f7487f9cbae01b96c
abdulrehmanazizf5 <abdulrehmanaziz5f@gmail.com>
2026-03-29 14:42:01 +0500

    feat: implement basic HTTP server and file system read/write utility

--- nodejs/code/server.js
+++ nodejs/code/server.js
@@ -1,14 +1,27 @@
 const fs = require("fs");
+const http = require("http");
 
-readWriteFile();
+// readWriteFile();
+createServer();
 
-async function readWriteFile() {
-  const readFileReturn = await fs.readFile(
-    "./server.txt",
-    "utf-8",
-    (err, data) => {
-      console.log(data);
-    },
-  );
-  fs.writeFileSync("./server.txt", "7pkr");
+function readWriteFile() {
+  const readFileReturn = fs.readFile("./server.txt", "utf-8", (err, data) => {
+    console.log(data);
+  });
+  fs.writeFileSync("./server.txt", "7pkr" + " readWriteFile");
+}
+
+function createServer() {
+  const server = http.createServer((req, res) => {
+    const pathname = req.url;
+    if (pathname !== "") {
+      res.end(`${pathname} wala page`);
+    } else {
+      console.log("something went wrong!");
+    }
+    // res.end("server is running -->");
+  });
+  server.listen(8000, "127.0.0.1", () => {
+    console.log("server is listening");
+  });
 }
